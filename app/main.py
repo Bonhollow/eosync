a17b9d86ffb25bd47f4e87b93962508197cac065
Bonhollow <bonollo99@gmail.com>
2025-07-21 21:57:33 +0200

    update: docker frontend expose

--- app/main.py
+++ app/main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 from core.config import settings
 from core.database import engine, Base
 from routers import employees, skills, projects, tasks, assignments, leaves, upload
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(employees.router, prefix="/employees", tags=["Employees"])
 app.include_router(skills.router, prefix="/skills", tags=["Skills"])
